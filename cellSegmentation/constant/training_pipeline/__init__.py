2d62afa39262f914cf279562b24449ab62262bcf
Petar Milivojevic <petar.milivojevic.biz@gmail.com>
2024-01-29 23:50:31 +0100

    data validation added

--- cellSegmentation/constant/training_pipeline/__init__.py
+++ cellSegmentation/constant/training_pipeline/__init__.py
@@ -5,3 +5,10 @@ DATA_INGESTION_DIR_NAME:str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
 
 DATA_DOWNLOAD_URL:str = "https://drive.google.com/file/d/17vDLJrk4iYtysocirKNCDuDIOAsX0-17/view?usp=sharing"
+
+
+DATA_VALIDATION_DIR_NAME:str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE:str = "status.txt"
+
+DATA_VALIDATION_ALL_RQUIRED_FILES = ["train", "valid", "test", "data.yaml"]
